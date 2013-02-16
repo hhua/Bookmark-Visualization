20c1d9a131bb5a2353550bb391247984a0b7b78e
chinesecold <chinesecold@gmail.com>
2013-02-16 13:29:36 -0500

    first commit

---
+++ pinboard.rb
@@ -0,0 +1,19 @@
+require 'net/http'
+require 'json'
+
+#avesh@cmu.edu
+
+def count_commnets(comment_array)
+	comment_array.size % 20
+end
+	
+def thread_data(board, tnum)
+	url = URI.parse('http://api.4chan.org/' + board.to_s + '/res/' + tnum.to_s + '.json')
+	return Net::HTTP.get(url)
+end
+
+#return the likelyhood of a meme from 1-10
+def meme_factor(board, tnum)
+	data = JSON.parse(thread_data(board, tnum))
+	count_comments(data["posts"])
+end
