5f471c898d81f761ceb4e1df26613cd991d3f7b6
chinesecold <chinesecold@gmail.com>
2013-02-18 01:24:50 -0500

    data finalized

--- ruby/handle_data.rb
+++ ruby/handle_data.rb
@@ -1,4 +1,7 @@
 require 'rexml/document'
+require 'json'
+
+# hhua.dev@gmail.com
 
 class Post
 	
@@ -34,11 +37,15 @@ post_doc.elements.each('posts/post') do |element|
 end
 
 tags_month = {}
+tag_post = {}
 for tag in tags
 	tag_month = {}
+	post_list = []
 	for post in posts
+		isTag = false
 		for each_tag in post.tags
 			if tag[0].eql? each_tag
+				isTag = true
 				date = post.time[0..6]	
 				if not tag_month[date]
 					tag_month[date] = 1
@@ -47,11 +54,57 @@ for tag in tags
 				end
 			end
 		end
+		if isTag
+			post_item = {}
+			post_item["description"] = post.description
+			post_item["href"] = post.href
+			post_list.push(post_item)
+		end
 	end
-	tags_month[tag[0]] = tag_month
+	tag_post[tag[0]] = post_list
+	tags_month[tag[0]] = tag_month 
 end
-puts tags_month
+# puts tags_month
+# puts tag_post
 
-#api_token = ARGV
-#xml = File.open("../data/tags.xml");
-#puts xml.read();
+# write into JSON file
+File.open("../viz/tag-post.json","w") do |f|
+	f.write(tag_post.to_json)
+end
+
+tag_detail = {}
+for key in tags_month.keys
+	month_count = tags_month[key]
+	month_list = []
+	for month in month_count.keys
+		each_month = {}		
+		each_month["month"] = month
+		each_month["count"] = month_count[month]
+		month_list.push(each_month)
+	end
+	tag_detail[key] = month_list
+end
+# puts tag_detail
+
+File.open("../viz/tag-detail.json","w") do |f|
+	f.write(tag_detail.to_json)
+end
+
+all_tags = []
+for tag in tags
+	detail = tag_detail[tag[0]]
+	for date in detail
+		entry = {}
+		entry["all_count"] = tag[1]
+		entry["month"] = date["month"]
+		entry["count"] = date["count"]
+		entry["tag"] = tag[0]
+		all_tags.push(entry)
+	end
+end
+#puts all_tags
+
+File.open("../viz/all-tags.json","w") do |f|
+	f.write(all_tags.to_json)
+end
+
